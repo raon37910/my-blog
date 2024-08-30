60cb7bdbac9e665426184732a0dd9aea334f15d1
김정민 <raon37910@gmail.com>
2024-08-30 20:29:22 +0900

    domain 추가

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,14 +1,14 @@
 export const USER_INFORMATIONS = {
-  "headerTitle": "Raon Notes",
-  "userName": "Raon",
-  "mascot": "🐯",
-  "resume": "",
-  "github": "https://github.com/raon37910",
-  "domain": "",
-  "meta": {
-    "title": "Raon Notes",
-    "description": "경험과 생각을 정리하는 노트입니다"
-  }
+  headerTitle: "Raon Notes",
+  userName: "Raon",
+  mascot: "🐯",
+  resume: "",
+  github: "https://github.com/raon37910",
+  domain: "https://my-blog-rho-lemon.vercel.app/",
+  meta: {
+    title: "Raon Notes",
+    description: "경험과 생각을 정리하는 노트입니다",
+  },
 };
 
 export const CATEGORY_ICONS: Record<string, string> = {
